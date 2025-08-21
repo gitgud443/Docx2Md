c7774abefb36d3f6cc9f5434f815e324f8127b79
gitgud443 <jeanvaljean3698@gmail.com>
2025-08-21 15:13:51 +0200

    Update build_docker_image.sh

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -6,8 +6,6 @@ docker build \
   --build-arg http_proxy=http://10.253.52.118/ \
   --build-arg https_proxy=http://10.253.52.118/ \
   --build-arg no_proxy=localhost,127.0.0.1,10.253.52.118 \
-  --build-arg NEXUS_LOGIN=arobin \
-  --build-arg NEXUS_PASSWORD='R_5525$3698nuar' \
   -t tech-doc-image-converter \
   -f Dockerfile.image-converter .
 
